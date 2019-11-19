418deda19c9c26596a671fe5682ec50d4ad51198
Kenneth <onegiantsfan25@yahoo.com>
2019-11-19 16:38:38 -0700

    displayed a button element rendering the state being passed down from parent container on props

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = ( number, onClick ) => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    /* Display a button element rendering the data being passed down from the parent container on props */
+    <button value = { number } className = { ( number === '0' ) ? 'tenth' : '' } onClick = { onClick }> { number } </button>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = ( character, onClick ) => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      /* Display a button element rendering the data being passed down from the parent container on props */
+  <button value = { character } onClick = { onClick } > { character } </button>
   );
 };
+
+export default SpecialButton;
